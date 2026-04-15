4369fd0eb99ad886fef69b9e83aa31d196f98899
tijana333 <150601653+tijana333@users.noreply.github.com>
2026-04-15 12:32:52 +0200

    form.ts

--- api/api.ts
+++ api/api.ts
@@ -69,45 +69,10 @@ export async function loadEntryByDate(
   };
 }
 
-export async function loadEntries(url: string): Promise<ApiResponse<Entry[]>> {
-  const response = await fetch(url);
-  const data = await response.json();
-
-  return {
-    status: response.status,
-    data,
-  };
-}
-
-export async function updateEntry(
-  id: string,
-  entry: EntryInput,
-): Promise<ApiResponse<any>> {
-  const response = await fetch(API_URL + "/" + id, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(entry),
-  });
-  const data = await response.json();
-  return {
-    status: response.status,
-    data,
-  };
-}
-
-export async function loadEntryByDate(
-  selectedDate: string,
-): Promise<ApiResponse<any>> {
-  const response = await fetch(API_URL + "?date=" + selectedDate);
-  const data = await response.json();
-  return {
-    status: response.status,
-    data,
-  };
-}
 export async function loadEntries(url: string): Promise<ApiResponse<any>> {
   const response = await fetch(url);
   const data = await response.json();
+
   return {
     status: response.status,
     data,
